58867c9a786a932d54a43638db532c9106d954e9
MaxKlaxxMiner <max@tetrisk.de>
2020-08-20 15:01:52 +0200

    v0.04: path: add background image + opacity

--- ts/svg-paths.ts
+++ ts/svg-paths.ts
@@ -5,18 +5,18 @@ class SvgPaths
   static run(): void
   {
     const defaultPaths =
-      'M  30 5\r\n' +
-      'H  70\r\n' +
-      'C  90  5  90  45   70  45\r\n' +
-      'C  50 45  50  15   30  35\r\n' +
-      'C  10 55  20   5   30   5\r\n' +
-      '\r\n' +
-      'M  30 55\r\n' +
-      'h  40\r\n' +
-      'c  20  0  20  40    0  40\r\n' +
-      's        -20 -30  -40 -10\r\n' +
-      '         -10 -30    0 -30\r\n' +
-      '\r\n';
+      "M  30 5\r\n" +
+      "H  70\r\n" +
+      "C  90  5  90  45   70  45\r\n" +
+      "C  50 45  50  15   30  35\r\n" +
+      "C  10 55  20   5   30   5\r\n" +
+      "\r\n" +
+      "M  30 55\r\n" +
+      "h  40\r\n" +
+      "c  20  0  20  40    0  40\r\n" +
+      "s        -20 -30  -40 -10\r\n" +
+      "         -10 -30    0 -30\r\n" +
+      "\r\n";
 
     const el = document.getElementById("txt") as HTMLTextAreaElement;
     if (!el) return;
@@ -29,7 +29,11 @@ class SvgPaths
     {
       const txt = el.value;
       if (txt === known) return;
-      prev.innerHTML = '<svg width="300" viewBox="0 0 100 100" stroke="currentColor"><path d="' + (known = txt).replace(/[&'"<>]/g, '') + '" /></svg>';
+      const size = 400;
+      prev.innerHTML =
+        "<svg width='" + size + "' viewBox='0 0 100 100' style='background:url(/img/test-a.png);background-repeat:no-repeat;background-size:" + size + "px;fill:#8cf8;stroke:#0488;stroke-width:.5px'>" +
+        "<path d='" + (known = txt).replace(/[&'"<>]/g, "") + "' />" +
+        "</svg>";
     }, 100);
   }
 }
